077cab719f895899f3568ea0b84fbeabdc94e43d
Griffin Ziegler <98347610+griffzig@users.noreply.github.com>
2023-12-14 10:34:36 -0600

    formatting

--- src/brick_breaker_game/Gameplay.java
+++ src/brick_breaker_game/Gameplay.java
@@ -5,13 +5,14 @@ import java.awt.*;
 import java.awt.event.*;
 
 class Gameplay extends JPanel implements KeyListener, ActionListener  {
+	// game components
 	private boolean play = true;
 	private int score = 0;
 	private int totalBricks = 32;
-	
 	private Timer timer;
 	private int delay = 8;
 	
+	// player components
 	private int player1 = 310;
 	private int ballXcoord = 120;
 	private int ballYcoord = 350;
@@ -32,21 +33,21 @@ class Gameplay extends JPanel implements KeyListener, ActionListener  {
 
 	public void paint(Graphics g) {
 		
-		//background color
+		// set background color
 		g.setColor(Color.black);
 		g.fillRect(1, 1, 692, 592);
 		
+		// paint the Bricks
 		bricks.paint((Graphics2D)g);
-		
 		g.fillRect(0, 0, 3, 592);
 		g.fillRect(0, 0, 692, 3);
 		g.fillRect(691, 0, 3, 592);
 		
-		//Paddle Color
+		// set Paddle Color
 		g.setColor(Color.YELLOW);
 		g.fillRect(player1, 550, 100, 12);
 		
-		//Ball Color
+		// set Ball Color
 		g.setColor(Color.white);  
 		g.fillOval(ballXcoord, ballYcoord, 10, 10);
 		
@@ -54,28 +55,32 @@ class Gameplay extends JPanel implements KeyListener, ActionListener  {
 		g.setFont(new Font("Sarif", Font.BOLD, 25));
 		g.drawString("Score: " + score, 520, 30);
 		
-		//Win Condition
+		// Win Condition
 		if (totalBricks <= 0) { 
+			// end the game
 			play = false;
 			ballXdir = 0;
 			ballYdir = 0;
+			
+			// Player Wins Screen
 			g.setColor(Color.GREEN);
 			g.setFont(new Font("Sarif", Font.BOLD, 30));
 			g.drawString("You Won, Score: " + score, 190, 300);
-			
 			g.setFont(new Font("Sarif", Font.BOLD, 20));
 			g.drawString("Press Enter to Restart.", 230, 350);
 		}
 		
-		//Game Over Condition
-		if(ballYcoord > 570) { 
+		// Game Over Condition
+		if (ballYcoord > 570) { 
+			// end the game
 			play = false;
 			ballXdir = 0;
 			ballYdir = 0;
+			
+			// Game Over Screen
 			g.setColor(Color.RED);
 			g.setFont(new Font("Sarif", Font.BOLD, 30));
 			g.drawString("Game Over, Score: " + score, 190, 300);
-			
 			g.setFont(new Font("Sarif", Font.BOLD, 20));
 			g.drawString("Press Enter to Restart", 230, 350);
 				
@@ -86,29 +91,33 @@ class Gameplay extends JPanel implements KeyListener, ActionListener  {
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		timer.start();
-		if(play) {
-			// Ball - Paddle  interaction 
-			if(new Rectangle(ballXcoord, ballYcoord, 20, 20).intersects(new Rectangle(player1, 550, 100, 8))) {
+		// Check if the game is in play mode
+		if (play) {
+			// Interaction between Ball and Paddle
+			if (new Rectangle(ballXcoord, ballYcoord, 20, 20).intersects(new Rectangle(player1, 550, 100, 8))) {
 				ballYdir = - ballYdir;
 			}
-			for( int i = 0; i<bricks.map.length; i++) { // Ball - Brick interaction
-				for(int j = 0; j<bricks.map[0].length; j++) {  // map.map[0].length is the number of columns
-					if(bricks.map[i][j] > 0) {
-						int brickX = j*bricks.brickWidth + 80;
-						int brickY = i*bricks.brickHeight + 50;
-						int brickWidth= bricks.brickWidth;
+			
+			// Interaction between Ball and Brick
+			for ( int i = 0; i < bricks.map.length; i++) { 
+				// 
+				for (int j = 0; j < bricks.map[0].length; j++) { 
+					if (bricks.map[i][j] > 0) {
+						int brickX = j * bricks.brickWidth + 80;
+						int brickY = i * bricks.brickHeight + 50;
+						int brickWidth = bricks.brickWidth;
 						int brickHeight = bricks.brickHeight;
 						
-						Rectangle rect = new Rectangle(brickX, brickY, brickWidth, brickHeight);
-						Rectangle ballRect = new Rectangle(ballXcoord, ballYcoord, 20,20);
-						Rectangle brickRect = rect;
+						Rectangle rectangle = new Rectangle(brickX, brickY, brickWidth, brickHeight);
+						Rectangle ballRectangle = new Rectangle(ballXcoord, ballYcoord, 20,20);
+						Rectangle brickRectangle = rectangle;
 						
-						if(ballRect.intersects(brickRect) ) {
+						if (ballRectangle.intersects(brickRectangle) ) {
 							bricks.setBrickValue(0, i, j);
 							totalBricks--;
-							score+=10;
+							score += 10;
 							
-							if(ballXcoord + 19 <= brickRect.x || ballXcoord +1 >= brickRect.x + brickRect.width) 
+							if (ballXcoord + 19 <= brickRectangle.x || ballXcoord +1 >= brickRectangle.x + brickRectangle.width) 
 								ballXdir = -ballXdir;
 							 else {
 								ballYdir = -ballYdir;
@@ -122,13 +131,18 @@ class Gameplay extends JPanel implements KeyListener, ActionListener  {
 			
 			ballXcoord += ballXdir;
 			ballYcoord += ballYdir;
-			if(ballXcoord < 0) { // if ball hits the left wall then it bounces back
+			
+			// determine ball action based on wall intersection
+			// left wall intersection
+			if (ballXcoord < 0) { 
 				ballXdir = -ballXdir;
 			}
-			if(ballYcoord < 0) {  // if ball hits the top wall then it bounces back
+			// top wall intersection
+			if (ballYcoord < 0) {  
 				ballYdir = -ballYdir;
 			}
-			if(ballXcoord > 670) { // if ball hits the right wall then it bounces back
+			// right wall intersection
+			if (ballXcoord > 670) { 
 				ballXdir = -ballXdir;  
 			
 			}
@@ -148,16 +162,19 @@ class Gameplay extends JPanel implements KeyListener, ActionListener  {
 	
 	@Override
 	public void keyPressed(KeyEvent arg0) {
-		if(arg0.getKeyCode() == KeyEvent.VK_RIGHT) { // right key pressed moves right
-			if(player1 >= 600) {
+		// right arrow key pressed = move right
+		if (arg0.getKeyCode() == KeyEvent.VK_RIGHT) { 
+			if (player1 >= 600) {
 				player1 = 600;
 			} else {
 				moveRight();
 					
 			}
 		}
-		if(arg0.getKeyCode() == KeyEvent.VK_LEFT) { // Left key pressed moves left
-			if(player1 < 10) {
+		
+		// left arrow key pressed = move left
+		if (arg0.getKeyCode() == KeyEvent.VK_LEFT) { 
+			if (player1 < 10) {
 				player1 = 10;
 			} else {
 				moveLeft();
@@ -165,8 +182,9 @@ class Gameplay extends JPanel implements KeyListener, ActionListener  {
 			}
 		}
 		
-		if(arg0.getKeyCode() == KeyEvent.VK_ENTER) { // Enter pressed restart
-			if(!play) {
+		// Enter key pressed = restart the game
+		if (arg0.getKeyCode() == KeyEvent.VK_ENTER) { 
+			if (!play) {
 				play = true;
 				ballXcoord = 120;
 				ballYcoord = 350;
@@ -181,11 +199,15 @@ class Gameplay extends JPanel implements KeyListener, ActionListener  {
 		}
 		
 	}	
-		public void moveRight() { // paddle moves right by 50 pixels
+	
+	// move the paddle right by 60 pixels
+	public void moveRight() { 
 			play = true;
 			player1 += 60;
 		}
-		public void moveLeft() { // paddle moves left by 50 pixels
+		
+	// move the paddle left by 60 pixels
+	public void moveLeft() { 
 			play = true;
 			player1 -= 60;
 		}
