23c7392fc3d7aac3b361b089ad4c0b64b4b6c092
griffzig <98347610+griffzig@users.noreply.github.com>
2023-12-14 13:33:47 -0600

    Formatting

--- src/brick_breaker_game/Main.java
+++ src/brick_breaker_game/Main.java
@@ -4,10 +4,12 @@ import java.awt.*;
 import javax.swing.*;
 
 
-public class Main{
+public class Main {
 	public static void main(String[] args) {
+		
 		JFrame frame = new JFrame();
 		Gameplay gameplay = new Gameplay();
+
 		frame.setBounds(10, 10, 700, 600);
 		frame.setTitle("Brick Breaker");
 		frame.setResizable(false);
